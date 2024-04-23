dbb88030d98f646d07a3e44efdfc0165d5b74f4d
gabrielruizmb <gabrielruizmb@gmail.com>
2024-04-22 21:09:01 -0300

    Início do funcionamento do carrossel

---
+++ front-end/components/home/home.js
@@ -0,0 +1,22 @@
+let mainContent = document.getElementById("main-content");
+
+function createCarouselItems(carouselItems) {
+    carouselItems.forEach(element => {
+        console.log(element);
+        let carouselItem = document.createElement("div");
+        carouselItem.innerHTML = `
+            <a href="${element.link}" target="blank">
+                <img class="carousel__item" src="${element.image}" >
+            </a>
+        `;
+        mainContent.appendChild(carouselItem);
+    });
+}
+
+async function loadCarouselItems() {
+    const response = await fetch("http://localhost:8081/carousel");
+    const carouselItems = await response.json();
+    createCarouselItems(carouselItems);
+}
+
+loadCarouselItems();

d44a420e3594265341878791cdcd3a2b6ddec3d4
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2024-06-13 11:09:25 -0300

    Adicionado login por Keycloak v1.0

--- front-end/components/system/sign/sign.js
+++ front-end/components/system/sign/sign.js
@@ -8,3 +8,33 @@ signInButton.addEventListener('click', flipTheSignCard);
 function flipTheSignCard() {
     signCard.classList.toggle('is-flipped');
 };
+
+alert("Teste");
+
+const requestObjParams = {
+    grant_type: 'password',
+    client_id: 'sublimoon',
+    username: 'gabrielruizmb',
+    password: 'user',
+    client_secret: '2jhu6nTDQbZncPRsqqzX4iyOZpul1jb0'
+};
+
+const requestOptions = {
+    method: 'POST',
+    headers:{'Content-Type':'application/x-www-form-urlencoded'},
+    body: 'grant_type=password&client_id=sublimoon&username=gabrielruizmb&password=user&client_secret=2jhu6nTDQbZncPRsqqzX4iyOZpul1jb0'
+};
+
+const request = new Request(
+    'http://192.168.56.101:8080/realms/development/protocol/openid-connect/token',
+    requestOptions
+);
+
+async function login() {
+    const response = await fetch(request);
+    const responseJson = response.json();
+    
+    console.log(responseJson);
+};
+
+login();

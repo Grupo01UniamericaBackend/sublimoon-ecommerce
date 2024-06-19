dbbd5e34b81d14ddef9729b881de28463ae0153d
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2024-06-18 21:37:05 -0300

    Adicionado sign-in com Keycloak v1.0

--- front-end/components/system/sign/sign.js
+++ front-end/components/system/sign/sign.js
@@ -1,3 +1,5 @@
+// Beginning of the code responsible for the 3d effect in sign-in/sign-up card.
+
 let signCard = document.querySelector('.sign-card');
 let signUpButton = document.querySelector('.sign-up-button');
 let signInButton = document.querySelector('.sign-in-button');
@@ -9,32 +11,70 @@ function flipTheSignCard() {
     signCard.classList.toggle('is-flipped');
 };
 
-alert("Teste");
+// End of the code responsible for the 3d effect in sign-in/sign-up card.
 
-const requestObjParams = {
-    grant_type: 'password',
-    client_id: 'sublimoon',
-    username: 'gabrielruizmb',
-    password: 'user',
-    client_secret: '2jhu6nTDQbZncPRsqqzX4iyOZpul1jb0'
-};
 
-const requestOptions = {
-    method: 'POST',
-    headers:{'Content-Type':'application/x-www-form-urlencoded'},
-    body: 'grant_type=password&client_id=sublimoon&username=gabrielruizmb&password=user&client_secret=2jhu6nTDQbZncPRsqqzX4iyOZpul1jb0'
-};
+let userMail = document.getElementById('mail');
+let userPassword = document.getElementById('password');
 
-const request = new Request(
-    'http://192.168.56.101:8080/realms/development/protocol/openid-connect/token',
-    requestOptions
-);
+let signFormEnterButton = document.getElementById('sign-form__enter-button');
+signFormEnterButton.addEventListener('click', signIn);
 
-async function login() {
-    const response = await fetch(request);
-    const responseJson = response.json();
-    
-    console.log(responseJson);
-};
+async function signIn() {
 
-login();
+    const response = await fetch(
+        'http://192.168.56.101:8080/realms/development/protocol/openid-connect/token',
+        {
+                method: 'POST',
+                headers:{'Content-Type':'application/x-www-form-urlencoded'},
+                body: new URLSearchParams({
+                client_id: 'sublimoon',
+                client_secret: '2jhu6nTDQbZncPRsqqzX4iyOZpul1jb0',
+                grant_type: 'password',
+                username: `${userMail.value}`,
+                password: `${userPassword.value}`
+             }).toString()
+        }
+    )
+    console.log(response.json());
+}
+
+// const requestOptions = {
+//     method: 'POST',
+//     headers:{'Content-Type':'application/x-www-form-urlencoded'},
+//     body: new URLSearchParams({
+//         client_id: 'sublimoon',
+//         client_secret: '2jhu6nTDQbZncPRsqqzX4iyOZpul1jb0',
+//         grant_type: 'password',
+//         username: `${userMail.value}`,
+//         password: `${userPassword.value}`
+//     }).toString()
+// };
+
+// const request = new Request(
+//     'http://192.168.56.101:8080/realms/development/protocol/openid-connect/token',
+//     {
+//         method: 'POST',
+//         headers:{'Content-Type':'application/x-www-form-urlencoded'},
+//         body: new URLSearchParams({
+//             client_id: 'sublimoon',
+//             client_secret: '2jhu6nTDQbZncPRsqqzX4iyOZpul1jb0',
+//             grant_type: 'password',
+//             username: `${userMail.value}`,
+//             password: `${userPassword.value}`
+//         }).toString()
+//     }
+// );
+
+// async function signIn() {
+//     const response = await fetch(request);
+//     const responseJson = response.json();
+//     console.log(`${userMail.value}`);
+//     console.log(`${userPassword.value}`);
+//     console.log(responseJson);
+// };
+
+// async function signIn() {
+//     alert(userMail.value);
+//     alert(userPassword.value);
+// }
